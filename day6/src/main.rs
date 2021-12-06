5ec34bb4d23b7e4c23b6e8cd4c80ff26b3f2ce3b
Nako11 <dawei2@mailbox.org>
2021-12-06 10:34:02 +0100

    day6

---
+++ day6/src/main.rs
@@ -0,0 +1,24 @@
+use std::collections::VecDeque;
+use std::fs::File;
+use std::io::{BufRead, BufReader};
+
+fn main() {
+    let file = File::open("input.txt").unwrap();
+    let mut line = String::from("");
+    BufReader::new(file).read_line(&mut line).ok();
+    let mut population: VecDeque<u64> = VecDeque::from([0; 9]);
+
+    for fish in line.trim().split(',').map(|v| v.parse::<usize>().unwrap()) {
+        population[fish] += 1;
+    }
+    for _ in 0..80 {
+        population.rotate_left(1);
+        population[6] += population[8];
+    }
+    println!("part_a n: {}", population.iter().sum::<u64>());
+    for _ in 80..256 {
+        population.rotate_left(1);
+        population[6] += population[8];
+    }
+    println!("part_b n: {}", population.iter().sum::<u64>());
+}
